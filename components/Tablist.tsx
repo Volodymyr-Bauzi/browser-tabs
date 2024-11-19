cc0138041059c8197681584bba650065ad900099
Volodymyr <volodiasyd@gmail.com>
2024-11-19 15:02:51 +0200

    added handleTabClick function

--- components/Tablist.tsx
+++ components/Tablist.tsx
@@ -4,17 +4,26 @@ import styles from './Tablist.module.css'
 
 interface TabListProps {
     tabs: Tab[];
-    activeTab: string | null
+    activeTab: string | null;
+    onTabChange: (tabId: string) => void;
+    onReorder: (updatedTabs: Tab[]) => void;
 }
 
-const TabList: React.FC<TabListProps> = ({tabs, activeTab}) => {
+const TabList: React.FC<TabListProps> = ({tabs, activeTab, onTabChange, onReorder}) => {
+
+    const handleTabClick = (tabId: string) => {
+        onTabChange(tabId)
+    }
+
     return (
         <div className={styles.tabList}>
             {tabs.map((tab) => (
                 <div key={tab.id}
                     className={`${styles.tab} ${tab.isPinned ? styles.pinned : ''} ${
                     activeTab === tab.id ? styles.active : ''
-                }`}>
+                    }`}
+                    onClick={() => handleTabClick(tab.id)}
+                >
                     {tab.label}
                 </div>
             ))}
